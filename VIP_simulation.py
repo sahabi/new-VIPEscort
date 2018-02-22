29921d1b8cf3b51e772856fa2069f183ffd3d219
sahabi <sahabi@gmail.com>
2018-02-22 08:49:00 -0600

    updated sim

--- VIP_simulation.py
+++ VIP_simulation.py
@@ -9,7 +9,7 @@ from inspect import getmembers, isfunction
 from libs.AvState import *
 from Vip import Vip
 from time import sleep
-
+from random import choice
 sys.path.insert(0, 'ctrl')
 
 class Location():
@@ -18,12 +18,6 @@ class Location():
         self.lat = lat
         self.lon = lon
 
-LOCATIONS = [0 for i in range(5)]
-LOCATIONS[0] = Location(45.2871,-121.0122) # bottom left
-LOCATIONS[1] = Location(45.2871,-120.9122) # bottom right
-LOCATIONS[2] = Location(45.3289,-120.9635) # middle
-LOCATIONS[3] = Location(45.3729,-121.0122) # top left
-LOCATIONS[4] = Location(45.3729,-120.9122) # top right
 AVERROR = 'Error: AirVehicleState for ID ' 
 
 context = zmq.Context()
@@ -36,19 +30,15 @@ socket_send.connect("tcp://127.0.0.1:5561")
 def fire_outputs(cmds, states):
     if cmds['vTrack1']:
         track(1)
-        #print("UAV 1 is tracking")
     else:
         goto(1,int(cmds['uloc1'])+1)
-        #print("UAV 1 goes to {}".format(int(cmds['uloc1'])+1))
     if cmds['vTrack2']:
         track(2)
-        #print("UAV 2 is tracking")
     else:
         goto(2,int(cmds['uloc2'])+1)
-        #print("UAV 2 goes to {}".format(int(cmds['uloc2'])+1))
     goto(4,int(cmds['vloc'])+1)
-    goto(3,5)
-    #print("VIP goes to {}".format(int(cmds['vloc'])+1))
+    enemy_loc = choice([3,4,5])
+    goto(3,enemy_loc)
 
 def goto(uav, loc):
     factory = LMCPFactory.LMCPFactory()
@@ -60,9 +50,6 @@ def goto(uav, loc):
              str(msg_obj.FULL_LMCP_TYPE_NAME) + "||0|0$"
     smsg = LMCPFactory.packMessage(msg_obj, True)
     socket_send.send(header + smsg)
-    #print '\nSending ' + msg_obj.FULL_LMCP_TYPE_NAME + ' for Air Vehicle ' + str(uav) + \
-    #          ' to perform goto task'
-    #print msg_obj.toXMLStr("")
 
 def track(tracker):
     factory = LMCPFactory.LMCPFactory()
@@ -77,41 +64,54 @@ def track(tracker):
 def update_avlocs(av_states, avlocs):
     for c,state in enumerate(av_states.values()):
         for i in range(5):
-            if in_(i, state):
+            (t, d) = in_(i, state)
+            if t:
                 avlocs[c] = i
                 break
     return avlocs
-    
+
 def update_inputs(splist, avlocs):
     return {'sp0':splist[0][0], 'sp1':splist[1][0], 'sp2':splist[2][0], 'sp3':splist[3][0], 
             'sp4':splist[4][0], 'vlocs':avlocs[3], 'uloc1s':avlocs[0], 'uloc2s':avlocs[1], 
             'olocs':avlocs[2]}
 
 def in_(sp,state):
-    if state.Latitude > LOCATIONS[sp].lat - 0.0192 and \
-       state.Latitude < LOCATIONS[sp].lat + 0.0192 and \
-       state.Longitude > LOCATIONS[sp].lon - 0.0192 and \
-       state.Longitude < LOCATIONS[sp].lon + 0.0192:
-           return True
+    width = 0.0192
+    LOCATIONS = [0 for i in range(5)]
+    LOCATIONS[0] = Location(45.2871,-121.0122) # bottom left
+    LOCATIONS[1] = Location(45.2871,-120.9122) # bottom right
+    LOCATIONS[2] = Location(45.3289,-120.9635) # middle
+    LOCATIONS[3] = Location(45.3729,-121.0122) # top left
+    LOCATIONS[4] = Location(45.3729,-120.9122) # top right
+    if state.Latitude > LOCATIONS[sp].lat - width and \
+      state.Latitude < LOCATIONS[sp].lat + width and \
+      state.Longitude > LOCATIONS[sp].lon - width and \
+      state.Longitude < LOCATIONS[sp].lon + width:
+          data = (str(state.ID) + " it is at " + str(state.Latitude) + ", " + str(state.Longitude),
+                state.Latitude - (LOCATIONS[sp].lat - width),
+                (LOCATIONS[sp].lat + width) - state.Latitude,
+                (LOCATIONS[sp].lon + width) - state.Longitude,
+                state.Longitude - (LOCATIONS[sp].lon - width))
+          return (True,data)
     else:
-        return False
+        return (False,0)
 
 def anyIn(sp,uavs):
     for uav in uavs.values():
-        if in_(sp[1],uav) and not uav.ID in [4,3]:
-            return True
+        if not uav.ID in [3,4]:
+            (t, d) = in_(sp[1],uav)
+            if t:
+                return True
     return False
 
 def isSurv(sp, av_states):
     if sp[0]:
         return True
     else:
-        #print anyIn(sp,av_states)
         return anyIn(sp,av_states)
 
 def update_surv(splist, av_states):
     splist = [(isSurv(sp,av_states),sp[1]) for sp in splist]
-    #print splist
     return splist
 
 def get_next_message(socket_sub, lmcp_factory):
@@ -159,7 +159,6 @@ def initialize_av_states(av_configurations, av_states, msg_obj, lmcp_factory, so
                 av_initializations.remove(msg_obj.get_ID())
             else:
                 print(av_error + str(msg_obj.get_ID()) + ' seen twice in one cycle.')
-                #sys.exit()
         msg_obj = get_next_message(socket_sub, lmcp_factory)
     return msg_obj
 
@@ -174,38 +173,21 @@ def update_av_states(av_states, msg_obj, lmcp_factory, socket_sub):
                 av_initializations.remove(msg_obj.get_ID())
             else:
                 print(AVERROR + str(msg_obj.get_ID()) + ' seen twice in one cycle.')
-                #sys.exit()
         msg_obj = get_next_message(socket_sub, lmcp_factory)
-    return msg_obj
+    return (msg_obj, av_states)
 
 def main():
     controller = Vip()
     splist = [(False,i) for i in range(5)]
-    avlocs = [0 for i in range(4)]
-    # Prepare LMCP factory
     lmcp_factory = LMCPFactory.LMCPFactory()
-
-    # Set up send and receive sockets to UxAS (must match ports in UxAS's
-    # cfg_<scenarioName>.xml file).
-    # Set up dictionaries to store AirVehicleConfigurations and AirVehicleStates key'd by ID.
     av_configurations = dict()
     av_states = dict()
     av_ids = set([1, 2, 3, 4])
-    # Record AirVehicleConfigurations. Stop once an AirVehicleState is seen.
-    # Ensure all expected UAVs referenced in Salty file are initialized. Warn about duplicates.
     msg_obj = initialize_av_configurations(av_configurations, av_ids, 
             lmcp_factory, socket_sub)
-    
-    # Pause to give time for UxAS to define tasks
-    time.sleep(3.0)
-
-    # Initialize dictionary of AvStates.
+    time.sleep(1.0)
     msg_obj = initialize_av_states(av_configurations, av_states, msg_obj, 
             lmcp_factory, socket_sub)
-
-    # ========================= Main Loop =========================
-    # Update input variables, move the controller, fire outputs,
-    # then update AirVehicleStates.
     avlocs = [0 for i in range(4)]
     while True:
         avlocs = update_avlocs(av_states, avlocs)
@@ -215,8 +197,10 @@ def main():
         output_state = controller.move(**input_states)
         print(output_state)
         fire_outputs(output_state, av_states)
-        sleep(18)
-        msg_obj = update_av_states(av_states, msg_obj, lmcp_factory, socket_sub)
+        for i in range(400):
+            avlocs = update_avlocs(av_states, avlocs)
+            splist = update_surv(splist,av_states)
+            (msg_obj, av_states) = update_av_states(av_states, msg_obj, lmcp_factory, socket_sub)
 
 if __name__ == '__main__':
     main()
